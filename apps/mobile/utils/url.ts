ac271d713f617641977a3272f51ff7afac1d6e30
Chriest Yu <jcppman@gmail.com>
2025-08-05 15:52:18 +0800

    ditch mobile

--- apps/mobile/utils/url.ts
+++
@@ -1,7 +0,0 @@
-import env from './env'
-
-export const generateAPIUrl = (relativePath: string) => {
-  const path = relativePath.startsWith('/') ? relativePath : `/${relativePath}`
-
-  return env.EXPO_PUBLIC_MANTA_API_URL.concat(path)
-}
